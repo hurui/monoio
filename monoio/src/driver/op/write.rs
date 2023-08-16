59bbe51a9c637e6e1d61cb3b8fd1bccf7133c351
Rui <huruidev@gmail.com>
2023-08-16 14:39:19 +0800

    Update write.rs

--- monoio/src/driver/op/write.rs
+++ monoio/src/driver/op/write.rs
@@ -63,6 +63,15 @@ impl<T: IoBuf> OpAble for Write<T> {
         let fd = self.fd.as_raw_fd();
         let seek_offset = libc::off_t::try_from(self.offset)
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "offset too big"))?;
+
+        #[cfg(target_os = "freebsd")]
+        return syscall_u32!(pwrite(
+            fd,
+            self.buf.read_ptr() as _,
+            self.buf.bytes_init(),
+            seek_offset
+        ));
+
         #[cfg(not(target_os = "macos"))]
         return syscall_u32!(pwrite64(
             fd,
