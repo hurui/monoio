966a80cb418a4e8c319a3b867b54dd93abaceb35
Rui <huruidev@gmail.com>
2023-08-16 14:44:16 +0800

    Update read.rs

--- monoio/src/driver/op/read.rs
+++ monoio/src/driver/op/read.rs
@@ -77,22 +77,6 @@ impl<T: IoBufMut> OpAble for Read<T> {
         let seek_offset = libc::off_t::try_from(self.offset)
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "offset too big"))?;
 
-        #[cfg(target_os = "freebsd")]
-        return syscall_u32!(pread(
-            fd,
-            self.buf.write_ptr() as _,
-            self.buf.bytes_total(),
-            seek_offset
-        ));
-        
-        #[cfg(not(target_os = "macos"))]
-        return syscall_u32!(pread64(
-            fd,
-            self.buf.write_ptr() as _,
-            self.buf.bytes_total(),
-            seek_offset
-        ));
-
         #[cfg(target_os = "macos")]
         return syscall_u32!(pread(
             fd,
